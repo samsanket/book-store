c79a15f17f067946b3594807fdc878efeb1c161c
sanket.deshpande <sanket.deshpande@tagrem.com>
2025-04-07 15:12:11 +0530

    inital comit

---
+++ catlog-service/src/test/java/com/labs/catlog_service/TestCatlogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.labs.catlog_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatlogServiceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.from(CatlogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
