2450b88848da5ad7dde03f0fdd1755d98f3d9a7c
sanket.deshpande <sanket.deshpande@tagrem.com>
2025-04-07 16:55:50 +0530

    catalog-service inintal commit .

--- catlog-service/src/test/java/com/labs/catlog_service/TestCatlogServiceApplication.java
+++ catlog-service/src/test/java/com/labs/catlog_service/TestCatlogServiceApplication.java
@@ -6,7 +6,9 @@ public class TestCatlogServiceApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.from(CatlogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(CatlogServiceApplication::main)
+				.with(TestcontainersConfiguration.class)
+				.run(args);
 	}
 
 }
