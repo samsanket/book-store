8f03bb87a0f85bd55a9da5c7b9cc37e7b6f8b6a1
samsanket <deshpandesanket49@gmail.com>
2025-04-08 23:21:09 +0530

    added order-service and rabbit MQ

---
+++ order-service/src/main/java/com/labs/order_service/domain/OrderNotFoundException.java
@@ -0,0 +1,11 @@
+package com.labs.order_service.domain;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public static OrderNotFoundException forOrderNumber(String orderNumber) {
+        return new OrderNotFoundException("Order with Number " + orderNumber + " not found");
+    }
+}
